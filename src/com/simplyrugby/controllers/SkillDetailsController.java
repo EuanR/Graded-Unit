4558600d1b8740ebd067972eed89bc72f3bafbd4
Euan <euanriggans1@gmail.com>
2018-04-22 00:52:55 +0100

    Added groundwork for SkillDetails
    -Added the groundwork for the Skill Details functionality to be added

---
+++ src/com/simplyrugby/controllers/SkillDetailsController.java
@@ -0,0 +1,67 @@
+package com.simplyrugby.controllers;
+
+import com.simplyrugby.exceptions.SkillCategoryNotFoundException;
+import com.simplyrugby.modals.Modal;
+import com.simplyrugby.objects.Skill;
+import com.simplyrugby.utils.Search;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author Euan
+ */
+public class SkillDetailsController {
+
+    Modal modal;
+    private String skillCategoryName;
+    private int playerID;
+
+    @FXML
+    private TableView tblSkillData;
+    @FXML
+    private Button btnSaveDetails;
+    @FXML
+    private AnchorPane pane;
+
+    public void setModal(Modal modal) {
+        this.modal = modal;
+    }
+
+    public void init(String skillCategoryName, int playerID) {
+        this.skillCategoryName = skillCategoryName;
+        this.playerID = playerID;
+        tblSkillData.setEditable(true);
+        TableColumn skillNameCol = new TableColumn("Skill Name");
+        skillNameCol.setPrefWidth(100);
+        skillNameCol.setCellValueFactory(new PropertyValueFactory<Skill, String>("skillName"));
+        skillNameCol.setCellFactory(TextFieldTableCell.<String>forTableColumn());
+        TableColumn skillRatingCol = new TableColumn("Skill Rating");
+        skillRatingCol.setPrefWidth(100);
+        skillRatingCol.setCellValueFactory(new PropertyValueFactory<Skill, Integer>("skillRating"));
+        skillNameCol.setCellFactory(TextFieldTableCell.<Integer>forTableColumn());
+        tblSkillData.getColumns().addAll(skillNameCol, skillRatingCol);
+        tblSkillData.setPrefSize(250, 200);
+        try {
+            ObservableList<Skill> dataToAdd = FXCollections.observableArrayList();
+            for (Skill skill : Search.getSkillCateogryFromName(skillCategoryName, playerID).getSkills()) {
+                dataToAdd.add(skill);
+            }
+            tblSkillData.setItems(dataToAdd);
+        } catch (SkillCategoryNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void btnSaveDetailsClickHandlder(javafx.event.ActionEvent event) {
+
+    }
+
+}
--- src/com/simplyrugby/controllers/SkillsMenuController.java
+++ src/com/simplyrugby/controllers/SkillsMenuController.java
@@ -1,12 +1,31 @@
 package com.simplyrugby.controllers;
 
+import com.simplyrugby.exceptions.PlayerNotFoundException;
 import com.simplyrugby.modals.Modal;
+import com.simplyrugby.objects.ComboBoxItem;
+import com.simplyrugby.objects.SkillCategory;
+import com.simplyrugby.utils.Search;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 /**
  * @author Euan
  */
 public class SkillsMenuController {
+
+    @FXML
+    private Button btnViewDetails;
+    @FXML
+    private ComboBox<ComboBoxItem> cmbSkillCategories;
 
     Modal modal;
     private int playerID;
@@ -18,5 +37,34 @@ public class SkillsMenuController {
     @FXML
     public void init(int playerID) {
         this.playerID = playerID;
+        try {
+            for (SkillCategory skillCategory : Search.getPlayerFromID(playerID).getSkills()) {
+                ComboBoxItem comboBoxItem = new ComboBoxItem(skillCategory.getCategory(), 0);
+                cmbSkillCategories.getItems().add(comboBoxItem);
+            }
+        } catch (PlayerNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void btnViewDetailsClickHandler(javafx.event.ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../views/SkillDetails.fxml"));
+            Parent root = null;
+            root = (Parent) fxmlLoader.load();
+            SkillDetailsController controller = fxmlLoader.getController();
+            controller.setModal(this.modal);
+            controller.init(cmbSkillCategories.getValue().getItemText(), playerID);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.DECORATED);
+            stage.setTitle("Skills Details -" + cmbSkillCategories.getValue().getItemText());
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
