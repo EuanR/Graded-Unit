6fc2a98b18ea2deaebb8cc6d0d2ae73dcab57c9c
Euan <euanriggans1@gmail.com>
2018-04-21 18:55:24 +0100

    Combo Box changes
    -Combo box now uses a players full name rather than just their ID.
    -Error handling in the case that a players ID is invalid.

--- src/com/simplyrugby/controllers/HomeController.java
+++ src/com/simplyrugby/controllers/HomeController.java
@@ -1,16 +1,23 @@
 package com.simplyrugby.controllers;
 
+import com.simplyrugby.exceptions.PlayerNotFoundException;
 import com.simplyrugby.modals.Modal;
+import com.simplyrugby.objects.Player;
 import com.simplyrugby.objects.Squad;
+import com.simplyrugby.utils.Search;
+import com.simplyrugby.utils.SimpleAlerts;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
 public class HomeController {
 
+    @FXML
+    private AnchorPane pane;
     @FXML
     private ComboBox cmbPlayers;
     @FXML
@@ -49,7 +56,14 @@ public class HomeController {
         }
         for (Squad squad : modal.getSquads()) {
             if (squad.getSquadName().toLowerCase().equals(squadName.toLowerCase())) {
-                cmbPlayers.getItems().addAll(squad.getPlayers());
+                for (int playerID : squad.getPlayers()) {
+                    try {
+                        Player tempPlayer = Search.getPlayerFromID(playerID + 10);
+                        cmbPlayers.getItems().add(tempPlayer.getFullName());
+                    } catch (PlayerNotFoundException e) {
+                        SimpleAlerts.simpleAlert(Alert.AlertType.ERROR, "Player not found", "A player with that ID does not exist").showAndWait();
+                    }
+                }
                 break;
             }
         }
