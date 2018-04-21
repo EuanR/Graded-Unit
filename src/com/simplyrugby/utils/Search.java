9fbfe4976e983d729685e405bdb0d5ddeaa498ad
Euan <euanriggans1@gmail.com>
2018-04-22 00:26:55 +0100

    Added a search function for SkillCategory
    -Added a search method within the search utility class to get the specified SkillCategory object from a user

--- src/com/simplyrugby/utils/Search.java
+++ src/com/simplyrugby/utils/Search.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.simplyrugby.exceptions.PlayerNotFoundException;
+import com.simplyrugby.exceptions.SkillCategoryNotFoundException;
 import com.simplyrugby.objects.Player;
+import com.simplyrugby.objects.SkillCategory;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -49,6 +51,34 @@ public class Search {
         }
         if (!found) {
             throw new PlayerNotFoundException();
+        }
+        return null;
+    }
+
+    public static SkillCategory getSkillCateogryFromName(String skillCategoryName, int playerID) throws SkillCategoryNotFoundException {
+        ArrayList<Player> tempPlayers = new ArrayList<>();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        boolean found = false;
+        Type playerType = new TypeToken<ArrayList<Player>>() {
+        }.getType();
+        try {
+            Reader reader = new FileReader("data/players.json");
+            tempPlayers = new Gson().fromJson(reader, playerType);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        for (Player player : tempPlayers) {
+            if (player.getUID() == playerID) {
+                for (SkillCategory skillCategory : player.getSkills()) {
+                    if (skillCategory.getCategory().equals(skillCategoryName)) {
+                        found = true;
+                        return skillCategory;
+                    }
+                }
+            }
+        }
+        if (!found) {
+            throw new SkillCategoryNotFoundException();
         }
         return null;
     }
