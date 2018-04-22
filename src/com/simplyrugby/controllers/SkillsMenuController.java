f761f58ffa6c164adedb36d2806ed4d5afcdedfc
Euan <euanriggans1@gmail.com>
2018-04-22 03:40:35 +0100

    Added functionality for skill category notes
    -The user is not able to view and add to a players notes regarding a specific skill category

--- src/com/simplyrugby/controllers/SkillsMenuController.java
+++ src/com/simplyrugby/controllers/SkillsMenuController.java
@@ -3,27 +3,32 @@ package com.simplyrugby.controllers;
 import com.simplyrugby.exceptions.PlayerNotFoundException;
 import com.simplyrugby.modals.Modal;
 import com.simplyrugby.objects.ComboBoxItem;
+import com.simplyrugby.objects.Player;
 import com.simplyrugby.objects.SkillCategory;
 import com.simplyrugby.utils.Search;
 import com.simplyrugby.utils.SimpleAlerts;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * @author Euan
  */
 public class SkillsMenuController {
 
+    @FXML
+    private Button btnAddNotes;
+    @FXML
+    private Button btnViewNotes;
     @FXML
     private Button btnViewDetails;
     @FXML
@@ -65,12 +70,82 @@ public class SkillsMenuController {
             Stage stage = new Stage();
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.initStyle(StageStyle.DECORATED);
-            stage.setTitle("Skills Details -" + cmbSkillCategories.getValue().getItemText());
+            stage.setTitle("Skills Details - " + cmbSkillCategories.getValue().getItemText());
             stage.setScene(new Scene(root));
             stage.setResizable(false);
             stage.showAndWait();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void btnAddNotesClickHandler() {
+        if (cmbSkillCategories.getSelectionModel().isEmpty()) {
+            SimpleAlerts.simpleAlert(Alert.AlertType.INFORMATION, "No skill category selected", "You need to select a skill category").showAndWait();
+            return;
+        }
+        TextInputDialog newNotes = new TextInputDialog();
+        Optional<String> tempSquadName = null;
+        Button cancel = (Button) newNotes.getDialogPane().lookupButton(ButtonType.CANCEL);
+        String notes = "";
+        cancel.addEventFilter(ActionEvent.ACTION, event ->
+                System.exit(0)
+        );
+        newNotes.setResizable(false);
+        try {
+            newNotes.setHeaderText("Please enter the note you wish to add for " + Search.getPlayerFromID(playerID).getFullName() + " regarding " + cmbSkillCategories.getValue().getItemText().toLowerCase() + " skill");
+        } catch (PlayerNotFoundException e) {
+            e.printStackTrace();
+        }
+        newNotes.setTitle("Please enter the note you wish to add");
+        tempSquadName = newNotes.showAndWait();
+        if (tempSquadName.isPresent()) {
+            notes = tempSquadName.get();
+            for (Player player : modal.getPlayers()) {
+                if (player.getUID() == playerID) {
+                    for (SkillCategory skillCategory : player.getSkills()) {
+                        if (skillCategory.getCategory().equals(cmbSkillCategories.getValue().getItemText())) {
+                            skillCategory.addNotes(notes);
+                            modal.exportSystemData();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @FXML
+    private void btnViewNotesClickHandler(javafx.event.ActionEvent event) {
+        if (cmbSkillCategories.getSelectionModel().isEmpty()) {
+            SimpleAlerts.simpleAlert(Alert.AlertType.INFORMATION, "No skill category selected", "You need to select a skill category").showAndWait();
+            return;
+        }
+        StringBuilder notesStringBuilder = new StringBuilder();
+        for (Player player : modal.getPlayers()) {
+            if (player.getUID() == playerID) {
+                for (SkillCategory skillCategory : player.getSkills()) {
+                    if (skillCategory.getCategory().equals(cmbSkillCategories.getValue().getItemText())) {
+                        for (String notes : skillCategory.getNotes()) {
+                            notesStringBuilder.append(notes);
+                            notesStringBuilder.append("\n");
+                        }
+                    }
+                }
+            }
+        }
+        if (notesStringBuilder.length() <= 0) {
+            try {
+                SimpleAlerts.simpleAlert(Alert.AlertType.INFORMATION, "No notes found", "No notes found for " + Search.getPlayerFromID(playerID).getFullName() + " regarding " + cmbSkillCategories.getValue().getItemText().toLowerCase() + " skill").showAndWait();
+            } catch (PlayerNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                SimpleAlerts.simpleAlert(Alert.AlertType.INFORMATION, "Notes for " + Search.getPlayerFromID(playerID).getFullName() + " regarding " + cmbSkillCategories.getValue().getItemText().toLowerCase() + " skill", notesStringBuilder.toString()).showAndWait();
+            } catch (PlayerNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
