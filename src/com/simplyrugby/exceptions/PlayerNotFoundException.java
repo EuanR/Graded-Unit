55c0e3fcd0b10b1c30d8fa64a385f50ca948b5d0
Euan <euanriggans1@gmail.com>
2018-04-24 14:41:15 +0100

    Javadocs for custom exceptions

--- src/com/simplyrugby/exceptions/PlayerNotFoundException.java
+++ src/com/simplyrugby/exceptions/PlayerNotFoundException.java
@@ -5,9 +5,17 @@ package com.simplyrugby.exceptions;
  */
 public class PlayerNotFoundException extends Exception {
 
+    /**
+     * Parameterless constructor
+     */
     public PlayerNotFoundException() {
     }
 
+    /**
+     * Exception with a custom message
+     *
+     * @param message Exception message
+     */
     public PlayerNotFoundException(String message) {
         super(message);
     }
--- src/com/simplyrugby/exceptions/SkillCategoryNotFoundException.java
+++ src/com/simplyrugby/exceptions/SkillCategoryNotFoundException.java
@@ -5,9 +5,17 @@ package com.simplyrugby.exceptions;
  */
 public class SkillCategoryNotFoundException extends Exception {
 
+    /**
+     * Parameterless constructor
+     */
     public SkillCategoryNotFoundException() {
     }
 
+    /**
+     * Exception with a custom message
+     *
+     * @param message Exception message
+     */
     public SkillCategoryNotFoundException(String message) {
         super(message);
     }
